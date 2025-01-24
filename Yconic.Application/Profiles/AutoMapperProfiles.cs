b5d4801cc5aae37573e4e2efc6545ad442ed0776
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-01-24 17:15:26 +0300

    Auto mappen has been implemented

---
+++ Yconic.Application/Profiles/AutoMapperProfiles.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Dtos.User;
+using Yconic.Domain.Models;
+
+namespace Yconic.Application.Profiles
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User,UserDto>()
+                .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.email, opt => opt.MapFrom(src => src.Email))
+                .ForMember(dest => dest.name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.surname,opt => opt.MapFrom(src => src.Surname))
+                .ForMember(dest => dest.role, opt => opt.MapFrom(src => src.Role))
+                .ForMember(dest => dest.age, opt => opt.MapFrom(src => src.Age))
+                .ForMember(dest => dest.weight, opt => opt.MapFrom(src => src.Weight))
+                .ForMember(dest => dest.height, opt => opt.MapFrom(src => src.Height))
+                .ForMember(dest => dest.phoneNumeber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.userPersonaId, opt => opt.MapFrom(src => src.UserPersonaId))
+                .ForMember(dest => dest.userGarderobeId, opt => opt.MapFrom(src => src.UserGarderobeId));
+        }
+    }
+}
