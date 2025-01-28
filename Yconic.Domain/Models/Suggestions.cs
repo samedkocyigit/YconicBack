5c1511ec311799dfb22985baae22cfb5b7ac4266
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-01-28 22:10:24 +0300

    User model field has been added to suggestion model

--- Yconic.Domain/Models/Suggestions.cs
+++ Yconic.Domain/Models/Suggestions.cs
@@ -1,7 +1,14 @@
-﻿namespace Yconic.Domain.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Yconic.Domain.Models;
 public class Suggestions
 {
     public Guid Id { get; set; }
-    public string Description { get; set; }
-    public string Image { get; set; }
+    public Guid UserId { get; set; }
+    public string? Description { get; set; }
+    public string? Image { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public ICollection<Clothe>? SuggestedLook { get; set; } = new List<Clothe>();
+    [JsonIgnore]
+    public User? User { get; set; }
 }
