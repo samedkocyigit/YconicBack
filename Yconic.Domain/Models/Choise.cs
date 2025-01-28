58863188cb491b6fae1377661fca2fda78173b19
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-01-29 01:13:50 +0300

    AiResponse models has been created

---
+++ Yconic.Domain/Models/Choise.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Models
+{
+    public class Choice
+    {
+        public Guid Id { get; set; }
+        public string? Text { get; set; }
+        public int? Index { get; set; }
+        [NotMapped]
+        public object? Logprobs { get; set; }
+        public string? FinishReason { get; set; }
+    }
+}
