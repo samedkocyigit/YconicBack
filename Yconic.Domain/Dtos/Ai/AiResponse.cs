880cdb8c796cc16c314602749d419c95260d072f
samedkocyigit <abdulsamed.kcygt@gmail.com>
2025-01-30 17:47:34 +0300

    new ai response models has been defined

---
+++ Yconic.Domain/Dtos/Ai/AiResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Dtos.Ai
+{
+    public class AiResponse
+    {
+        [JsonPropertyName("choices")]
+        public List<Choice> Choices { get; set; }
+
+        [JsonPropertyName("usage")]
+        public Usage Usage { get; set; }
+    }
+
+}
+
+
+
---
+++ Yconic.Domain/Dtos/Ai/Choise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Dtos.Ai
+{
+    public class Choice
+    {
+        [JsonPropertyName("message")]
+        public AiMessage Message { get; set; }
+    }
+
+}
