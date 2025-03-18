e2b56e333e73d4917eb62f59e5ace3a570b211f0
sam <abdulsamed.kcygt@gmail.com>
2025-03-18 23:12:57 +0300

    new mapper func has been defined for aiRequest

--- Yconic.Application/Profiles/AutoMapperProfiles.cs
+++ Yconic.Application/Profiles/AutoMapperProfiles.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yconic.Domain.Dtos.Ai;
 using Yconic.Domain.Dtos.User;
 using Yconic.Domain.Models;
 
@@ -13,6 +14,24 @@ namespace Yconic.Application.Profiles
     {
         public AutoMapperProfiles()
         {
+            CreateMap<Clothe, ClotheItemDto>()
+                .ForMember(dest => dest.clotheId, opt => opt.MapFrom(src => src.Id.ToString()))
+                .ForMember(dest => dest.image_path, opt => opt.MapFrom(src => src.MainPhoto));
+
+            CreateMap<Garderobe, GarderobeDto>()
+                .ForMember(dest => dest.categories, opt => opt.MapFrom(src =>
+                    src.ClothesCategory.ToDictionary(
+                        cat => cat.Name,
+                        cat => cat.Clothes.ToList()
+                    )
+                ));
+
+            CreateMap<UserDto, AiUserDto>()
+                .ForMember(dest => dest.persona, opt => opt.MapFrom(src => src.persona.Usertype))
+                .ForMember(dest => dest.userGarderobe, opt => opt.MapFrom(src => src.garderobe))
+                .ReverseMap();
+
+
             CreateMap<User,UserDto>()
                 .ForMember(dest => dest.id, opt => opt.MapFrom(src => src.Id))
                 .ForMember(dest => dest.email, opt => opt.MapFrom(src => src.Email))
@@ -22,11 +41,12 @@ namespace Yconic.Application.Profiles
                 .ForMember(dest => dest.age, opt => opt.MapFrom(src => src.Age))
                 .ForMember(dest => dest.weight, opt => opt.MapFrom(src => src.Weight))
                 .ForMember(dest => dest.height, opt => opt.MapFrom(src => src.Height))
-                .ForMember(dest => dest.phoneNumeber, opt => opt.MapFrom(src => src.PhoneNumber))
+                .ForMember(dest => dest.phoneNumber, opt => opt.MapFrom(src => src.PhoneNumber))
                 .ForMember(dest => dest.userPersonaId, opt => opt.MapFrom(src => src.UserPersonaId))
                 .ForMember(dest => dest.userGarderobeId, opt => opt.MapFrom(src => src.UserGarderobeId))
                 .ForMember(dest => dest.garderobe,opt => opt.MapFrom(src => src.UserGarderobe))
                 .ForMember(dest => dest.persona, opt => opt.MapFrom(src=> src.UserPersona));
+
         }
     }
 }
