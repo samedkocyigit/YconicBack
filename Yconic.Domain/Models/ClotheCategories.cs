8e4aee00616631ac9e6ead7a1e74bd23ebcac85a
sam <abdulsamed.kcygt@gmail.com>
2025-03-19 16:21:26 +0300

    ClotheTypes has been added to ClotheCategories

--- Yconic.Domain/Models/ClotheCategories.cs
+++ Yconic.Domain/Models/ClotheCategories.cs
@@ -6,7 +6,7 @@ public class ClotheCategories
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public CategoryTypes CategoryType {get; set;}
     public Guid GarderobeId { get; set; }
     [JsonIgnore]
     public Garderobe? Garderobe { get; set; }
