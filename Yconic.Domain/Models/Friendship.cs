451818850c01037e1e28ab9e5ce459c6e3c769d4
sam <abdulsamed.kcygt@gmail.com>
2025-03-29 16:14:45 +0300

    new model for users friendships

---
+++ Yconic.Domain/Models/Friendship.cs
@@ -0,0 +1,19 @@
+﻿using Yconic.Domain.Enums;
+
+namespace Yconic.Domain.Models
+{
+    public class Friendship
+    {
+        public Guid Id { get; set; }
+
+        public Guid RequesterId { get; set; }
+        public User Requester { get; set; }
+
+        public Guid AddresseeId { get; set; }
+        public User Addressee { get; set; }
+
+        public FriendshipStatus Status { get; set; } = FriendshipStatus.Pending;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
