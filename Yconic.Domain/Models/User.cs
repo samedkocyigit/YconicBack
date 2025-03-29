9d19b7ec64ddd0c1d808728ae1e834613b6994b7
sam <abdulsamed.kcygt@gmail.com>
2025-03-29 16:15:06 +0300

    update

--- Yconic.Domain/Models/User.cs
+++ Yconic.Domain/Models/User.cs
@@ -8,21 +8,31 @@ namespace Yconic.Domain.Models;
         public string Email { get; set; }
         public string Password { get; set; }
         public string Surname { get; set; }
-        public string? PasswordResetToken { get; set; }
-        public DateTime? PasswordResetTokenExpiry { get; set; }
-        public UserRoles Role { get; set; } = UserRoles.User;
         public DateTime? Birthday { get; set; }
+        public UserRoles Role { get; set; } = UserRoles.User;
+        public bool IsPrivate { get; set; } = false; 
         public int? Age { get; set; }
         public decimal? Height { get; set; }
         public decimal? Weight { get; set; }
         public string PhoneNumber { get; set; }
+        
+        public string? PasswordResetToken { get; set; }
+        public DateTime? PasswordResetTokenExpiry { get; set; }
+    
         public Status IsActive { get; set; } = Status.Active;
+
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
+
         public Guid? UserPersonaId { get; set; }
-        public Guid? UserGarderobeId { get; set; }
         public Persona? UserPersona { get; set; }
+    
+        public Guid? UserGarderobeId { get; set; }
         public Garderobe? UserGarderobe { get; set; }
+
         public ICollection<Suggestions>? Suggestions { get; set; } = new List<Suggestions>();
-    }
+        public ICollection<Friendship> FriendsSent { get; set; } = new List<Friendship>();     
+        public ICollection<Friendship> FriendsReceived { get; set; } = new List<Friendship>(); 
+
+}
