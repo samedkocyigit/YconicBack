634c84ef85cdb7f58b654936ffb84d629854ecdb
sam <abdulsamed.kcygt@gmail.com>
2025-04-04 12:34:15 +0300

    new models created

---
+++ Yconic.Domain/Models/Follow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Models
+{
+    public class Follow
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public Guid FollowerId { get; set; }
+        [JsonIgnore]
+        public User? Follower { get; set; }
+
+        public Guid FollowedId { get; set; }
+        [JsonIgnore]
+        public User? Followed { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
---
+++ Yconic.Domain/Models/FollowRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+using Yconic.Domain.Enums;
+
+namespace Yconic.Domain.Models
+{
+    public class FollowRequest
+    {
+        public Guid Id { get; set; }
+
+        public Guid RequesterId { get; set; }
+        [JsonIgnore]
+        public User? Requester { get; set; }
+
+        public Guid TargetUserId { get; set; }
+        [JsonIgnore]
+        public User? TargetUser { get; set; }
+
+        public DateTime RequestedAt { get; set; } = DateTime.UtcNow;
+        public bool IsApproved { get; set; } = false;
+        public bool IsRejected { get; set; } = false;
+    }
+
+}
