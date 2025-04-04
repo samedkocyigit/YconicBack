9e8b6518e9dcdf6e894adc9fac7f0ebdef895144
sam <abdulsamed.kcygt@gmail.com>
2025-04-04 12:24:35 +0300

    follow controller has been created

---
+++ Yconic.API/Controllers/FollowController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Yconic.Application.Services.FollowServices;
+
+namespace Yconic.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FollowController : ControllerBase
+    {
+        private readonly IFollowService _followService;
+
+        public FollowController(IFollowService followService)
+        {
+            _followService = followService;
+        }
+
+        [HttpPost("{followerId}/follow/{followedId}")]
+        public async Task<IActionResult> Follow(Guid followerId, Guid followedId)
+        {
+            var result = await _followService.FollowUser(followerId, followedId);
+            return Ok(result);
+        }
+
+        [HttpDelete("{followerId}/unfollow/{followedId}")]
+        public async Task<IActionResult> Unfollow(Guid followerId, Guid followedId)
+        {
+            var result = await _followService.UnfollowUser(followerId, followedId);
+            return Ok(result);
+        }
+
+        [HttpGet("{userId}/followers")]
+        public async Task<IActionResult> GetFollowers(Guid userId)
+        {
+            var result = await _followService.GetFollowers(userId);
+            return Ok(result);
+        }
+
+        [HttpGet("{userId}/following")]
+        public async Task<IActionResult> GetFollowing(Guid userId)
+        {
+            var result = await _followService.GetFollowing(userId);
+            return Ok(result);
+        }
+    }
+}
