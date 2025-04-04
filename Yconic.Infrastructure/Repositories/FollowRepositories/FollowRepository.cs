3afb7d965d5baa116f791621ab22adef1882f6fb
sam <abdulsamed.kcygt@gmail.com>
2025-04-04 12:35:31 +0300

    follow repositories has been defined and implemented

---
+++ Yconic.Infrastructure/Repositories/FollowRepositories/FollowRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Yconic.Domain.Models;
+using Yconic.Infrastructure.ApplicationDbContext;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.FollowRepositories
+{
+    public class FollowRepository : GenericRepository<Follow>, IFollowRepository
+    {
+        private readonly AppDbContext _context;
+
+        public FollowRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<Follow> GetFollow(Guid followerId, Guid followedId)
+        {
+            return await _context.Follows.FirstOrDefaultAsync(f =>
+                f.FollowerId == followerId && f.FollowedId == followedId);
+        }
+        public async Task<bool> ExistsAsync(Guid followerId, Guid followedId)
+        {
+            return await _context.Follows.AnyAsync(f =>
+                f.FollowerId == followerId && f.FollowedId == followedId);
+        }
+
+        public async Task<List<Follow>> GetFollowers(Guid userId)
+        {
+            return await _context.Follows
+                .Include(f => f.Follower)
+                .Where(f => f.FollowedId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<List<Follow>> GetFollowing(Guid userId)
+        {
+            return await _context.Follows
+                .Include(f => f.Followed)
+                .Where(f => f.FollowerId == userId)
+                .ToListAsync();
+        }
+    }
+}
---
+++ Yconic.Infrastructure/Repositories/FollowRepositories/IFollowRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Models;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.FollowRepositories
+{
+    public interface IFollowRepository : IGenericRepository<Follow>
+    {
+        Task<bool> ExistsAsync(Guid followerId, Guid followedId);
+        Task<List<Follow>> GetFollowers(Guid userId);
+        Task<List<Follow>> GetFollowing(Guid userId);
+        Task<Follow> GetFollow(Guid followerId, Guid followedId);
+    }
+
+}
