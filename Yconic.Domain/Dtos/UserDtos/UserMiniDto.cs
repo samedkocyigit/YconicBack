6154c1663bfd3e4a468eb453b67622de03cacb97
sam <abdulsamed.kcygt@gmail.com>
2025-04-04 12:33:52 +0300

    new dtos for user

---
+++ Yconic.Domain/Dtos/UserDtos/UserMiniDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Dtos.UserDtos
+{
+    public class UserMiniDto
+    {
+        public Guid id { get; set; }
+        public string username { get; set; }
+        public string? profilePhoto { get; set; }
+    }
+
+}
