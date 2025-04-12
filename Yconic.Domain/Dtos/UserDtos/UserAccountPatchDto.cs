85e90b07f9f86922ba01e10ed8029acd1e481423
sam <abdulsamed.kcygt@gmail.com>
2025-04-12 22:26:41 +0300

    removed unused prop

--- Yconic.Domain/Dtos/UserDtos/UserAccountPatchDto.cs
+++ Yconic.Domain/Dtos/UserDtos/UserAccountPatchDto.cs
@@ -8,7 +8,6 @@ namespace Yconic.Domain.Dtos.UserDtos
 {
     public class UserAccountPatchDto
     {
-        public string? email { get; set; }
         public string? phoneNumber { get; set; }
         public DateTime? birthday  { get; set; }
         public decimal? height { get; set; }
