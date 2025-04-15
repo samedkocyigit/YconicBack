14d8f1406f168ab9c7eb064420d965bfe113e805
sam <abdulsamed.kcygt@gmail.com>
2025-04-15 14:36:20 +0300

    SharedLookLike repositories has been defined and implemented

---
+++ Yconic.Infrastructure/Repositories/SharedLookLikeRepositories/ISharedLookLikeRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Models;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.SharedLookLikeRepositories
+{
+    public interface ISharedLookLikeRepository:IGenericRepository<SharedLookLike>
+    {
+        Task<IEnumerable<SharedLookLike>> GetSharedLookLikesBySharedLookId(Guid sharedLookId);
+        Task<IEnumerable<SharedLookLike>> GetByIdWithUser(Guid userId);
+        Task<bool> IsLikeExist(Guid sharedLookId, Guid userId);
+        Task<SharedLookLike> GetExistingLike(Guid sharedLookId, Guid userId);
+    }
+}
---
+++ Yconic.Infrastructure/Repositories/SharedLookLikeRepositories/SharedLookLikeRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Models;
+using Yconic.Infrastructure.ApplicationDbContext;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.SharedLookLikeRepositories
+{
+    public class SharedLookLikeRepository: GenericRepository<SharedLookLike> , ISharedLookLikeRepository
+    {
+        private readonly AppDbContext _context;
+        public SharedLookLikeRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<SharedLookLike>> GetSharedLookLikesBySharedLookId(Guid sharedLookId)
+        {
+            return await _context.SharedLookLikes
+                .Include(x => x.LikedUser)
+                .Where(x => x.LikedSharedLookId == sharedLookId && x.IsLiked == true)
+                .ToListAsync();
+        }
+        public async Task<IEnumerable<SharedLookLike>> GetByIdWithUser(Guid userId)
+        {
+            return await _context.SharedLookLikes
+                .Include(x => x.LikedUser)
+                .Where(x => x.LikedUserId == userId)
+                .ToListAsync();
+        }
+        
+        public async Task<bool> IsLikeExist(Guid sharedLookId, Guid userId)
+        {
+            return await _context.SharedLookLikes
+                .AnyAsync(x => x.LikedSharedLookId == sharedLookId && x.LikedUserId == userId);
+        }
+
+        public async Task<SharedLookLike> GetExistingLike(Guid sharedLookId, Guid userId)
+        {
+            return await _context.SharedLookLikes
+                .FirstOrDefaultAsync(x => x.LikedSharedLookId == sharedLookId && x.LikedUserId == userId);
+        }
+    }
+}
