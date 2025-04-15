3d5889577f37a5c1d740e079c14fed636948c66f
sam <abdulsamed.kcygt@gmail.com>
2025-04-15 14:39:23 +0300

    removed unused line

--- Yconic.Domain/Dtos/SuggestionDtos/SimpleSuggestionDto.cs
+++ Yconic.Domain/Dtos/SuggestionDtos/SimpleSuggestionDto.cs
@@ -9,7 +9,6 @@ namespace Yconic.Domain.Dtos.SuggestionDtos
     public class SimpleSuggestionDto
     {
         public Guid id { get; set; }
-        public Guid suggestionId { get; set; }
         public Guid userId { get; set; }
         public string mainImage { get; set; }
     }
