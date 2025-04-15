485ac4db0e38db4fc2051a5828453da7aac9c188
sam <abdulsamed.kcygt@gmail.com>
2025-04-15 14:40:15 +0300

    Like dtos has been defined

---
+++ Yconic.Domain/Dtos/LikeDtos/CreateSharedLookLikeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Dtos.LikeDtos
+{
+    public class CreateSharedLookLikeDto
+    {
+        public Guid userId { get; set; }
+        public Guid sharedLookId { get; set; }
+
+    }
+}
---
+++ Yconic.Domain/Dtos/LikeDtos/LikeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Dtos.LikesDtos
+{
+    public class LikeDto
+    {
+        public Guid id { get; set; }
+        public Guid userId { get; set; }
+        public string userPhoto { get; set; }
+        public string username { get; set; }
+    }
+}
