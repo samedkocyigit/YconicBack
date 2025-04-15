8f3b94e3fbdea9afac07b6ff843bf392951fb8e0
sam <abdulsamed.kcygt@gmail.com>
2025-04-15 14:38:16 +0300

    SharedLookReview model created

---
+++ Yconic.Domain/Models/SharedLookReview.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Models
+{
+    public class SharedLookReview
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ReviewerUserId { get; set; }
+        public Guid ReviewedSharedLookId { get; set; }
+        public required string Review { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public User? ReviewerUser { get; set; }
+        public SharedLook? ReviewedSharedLook { get; set; }
+
+    }
+}
