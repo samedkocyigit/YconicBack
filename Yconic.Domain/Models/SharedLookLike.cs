17554208027493e32fcf277a2f9e84503a09fa95
sam <abdulsamed.kcygt@gmail.com>
2025-04-15 14:38:02 +0300

    sharedLookLike model created

---
+++ Yconic.Domain/Models/SharedLookLike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yconic.Domain.Models
+{
+    public class SharedLookLike
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid LikedUserId { get; set; }
+        public Guid LikedSharedLookId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsLiked { get; set; } = true;   
+        public User? LikedUser { get; set; }
+        public SharedLook? LikedSharedLook { get; set; }
+    }
+}
