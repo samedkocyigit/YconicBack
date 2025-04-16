ce5a0c34f142bc7c77c92aea8e8cff95870f87df
sam <abdulsamed.kcygt@gmail.com>
2025-04-16 21:05:41 +0300

    refactored

--- Yconic.Domain/Dtos/SharedLookDtos/SharedLookDetailDto.cs
+++ Yconic.Domain/Dtos/SharedLookDtos/SharedLookDetailDto.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Yconic.Domain.Dtos.LikesDtos;
 using Yconic.Domain.Dtos.ReviewDtos;
+using Yconic.Domain.Dtos.UserDtos;
 using Yconic.Domain.Models;
 
 namespace Yconic.Domain.Dtos.SharedLookDtos
@@ -14,13 +15,14 @@ namespace Yconic.Domain.Dtos.SharedLookDtos
         public Guid id { get; set; }
         public string mainUrlPhoto { get; set; }
         public string description { get; set; }
+        public string userId { get; set; }
         public string username { get; set; }
         public string userPhoto { get; set; }
         public DateTime createdAt { get; set; }
-        public List<Clothe> photos { get; set; }
+        public List<Clothe> clothes { get; set; }
         public int likesCount { get; set; }
-        public int reviewCount { get; set; }
-        public List<LikeDto> likes { get; set; }
+        public int reviewsCount { get; set; }
+        public List<UserMiniDto> likes { get; set; }
         public List<ReviewDto> reviews { get; set; }
     }
 }
