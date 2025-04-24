2790bdd6d936379577a06de9fc8d1490149456cd
sam <abdulsamed.kcygt@gmail.com>
2025-04-24 11:28:47 +0300

    new models has been created for more modular structure

---
+++ Yconic.Domain/Models/UserModels/UserPhysical.cs
@@ -0,0 +1,12 @@
+﻿namespace Yconic.Domain.Models.UserModels
+{
+    public class UserPhysical : BaseClass
+    {
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+        public decimal? Height { get; set; }
+        public decimal? Weight { get; set; }
+        public int? Age { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
