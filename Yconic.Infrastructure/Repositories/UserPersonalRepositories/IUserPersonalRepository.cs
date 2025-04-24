a05f795a0643f9243cdae88f68e8752007d13df6
sam <abdulsamed.kcygt@gmail.com>
2025-04-24 11:32:05 +0300

    new repositories has been created

---
+++ Yconic.Infrastructure/Repositories/UserPersonalRepositories/IUserPersonalRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Models.UserModels;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.UserPersonalRepositories
+{
+    public interface IUserPersonalRepository : IGenericRepository<UserPersonal>
+    {
+        Task<UserPersonal> GetUserPersonalByUserId(Guid userId);
+    }
+}
---
+++ Yconic.Infrastructure/Repositories/UserPersonalRepositories/UserPersonalRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Models.UserModels;
+using Yconic.Infrastructure.ApplicationDbContext;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.UserPersonalRepositories
+{
+    public class UserPersonalRepository : GenericRepository<UserPersonal>, IUserPersonalRepository
+    {
+        protected readonly AppDbContext _context;
+
+        public UserPersonalRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserPersonal> GetUserPersonalByUserId(Guid userId)
+        {
+            return await _context.UserPersonals
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+        }
+    }
+}
