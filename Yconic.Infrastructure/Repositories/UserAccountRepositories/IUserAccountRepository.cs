5a42b5c0c719971523f1cb639dcbe4d882316c1c
sam <abdulsamed.kcygt@gmail.com>
2025-04-25 14:19:25 +0300

    new queries has been defined and implemented

--- Yconic.Infrastructure/Repositories/UserAccountRepositories/IUserAccountRepository.cs
+++ Yconic.Infrastructure/Repositories/UserAccountRepositories/IUserAccountRepository.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yconic.Domain.Models.UserModels;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
 
 namespace Yconic.Infrastructure.Repositories.UserAccountRepositories
 {
-    interface IUserAccountRepository
+    public interface IUserAccountRepository : IGenericRepository<UserAccount>
     {
+        Task<UserAccount> GetUserAccountByUserId(Guid userId);
+
+        Task<UserAccount> GetUserAccountById(Guid id);
     }
 }
--- Yconic.Infrastructure/Repositories/UserAccountRepositories/UserAccountRepository.cs
+++ Yconic.Infrastructure/Repositories/UserAccountRepositories/UserAccountRepository.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yconic.Domain.Models.UserModels;
+using Yconic.Infrastructure.ApplicationDbContext;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
 
 namespace Yconic.Infrastructure.Repositories.UserAccountRepositories
 {
-    internal class UserAccountRepository
+    public class UserAccountRepository : GenericRepository<UserAccount>, IUserAccountRepository
     {
+        protected readonly AppDbContext _context;
+
+        public UserAccountRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public Task<UserAccount> GetUserAccountByUserId(Guid userId)
+        {
+            return _context.UserAccounts
+                .FirstOrDefaultAsync(x => x.UserId == userId);
+        }
+
+        public Task<UserAccount> GetUserAccountById(Guid id)
+        {
+            return _context.UserAccounts
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
