15e077bc6030e61ff1d76cec4afa2fd346836ac0
sam <abdulsamed.kcygt@gmail.com>
2025-04-25 14:15:31 +0300

    new models has been defined and created relations

--- Yconic.Infrastructure/ApplicationDbContext/AppDbContext.cs
+++ Yconic.Infrastructure/ApplicationDbContext/AppDbContext.cs
@@ -11,31 +11,72 @@ public class AppDbContext : DbContext
     {
     }
 
-    public DbSet<Persona> Personas { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<UserAccount> UserAccounts { get; set; }
+    public DbSet<UserPersonal> UserPersonals { get; set; }
+    public DbSet<UserPhysical> UserPhysicals { get; set; }
+    public DbSet<Persona> Personas { get; set; }
+    public DbSet<PersonaType> PersonaTypes { get; set; }
     public DbSet<Garderobe> Garderobes { get; set; }
     public DbSet<ClotheCategory> ClotheCategories { get; set; }
+    public DbSet<ClotheCategoryType> ClotheCategoryTypes { get; set; }
+    public DbSet<Clothe> Clothes { get; set; }
+    public DbSet<ClothePhoto> ClothePhotos { get; set; }
     public DbSet<Suggestion> Suggestions { get; set; }
     public DbSet<SharedLook> SharedLooks { get; set; }
     public DbSet<SharedLookLike> SharedLookLikes { get; set; }
     public DbSet<SharedLookReview> SharedLookReviews { get; set; }
-    public DbSet<Clothe> Clothes { get; set; }
-    public DbSet<ClothePhoto> ClothePhotos { get; set; }
     public DbSet<Follow> Follows { get; set; }
     public DbSet<FollowRequest> FollowRequests { get; set; }
-    public DbSet<UserAccount> UserAccounts { get; set; }
-    public DbSet<UserPersonal> UserPersonals { get; set; }
-    public DbSet<UserPhysical> UserPhysicals { get; set; }
-    public DbSet<ClotheCategoryType> ClotheCategoryTypes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Configure UserAccount
+
+        modelBuilder.Entity<UserAccount>(entity =>
+        {
+            entity.HasKey(ua => ua.Id);
+            entity.Property(ua => ua.PhoneNumber).HasMaxLength(15);
+            entity.Property(ua => ua.IsPrivate).HasConversion<string>();
+            entity.Property(ua => ua.EmailVerified).HasConversion<string>();
+            entity.Property(ua => ua.PhoneVerified).HasConversion<string>();
+            entity.HasOne(ua => ua.User)
+                  .WithOne(u => u.UserAccount)
+                  .HasForeignKey<UserAccount>(ua => ua.UserId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // Configure UserPersonal
+        modelBuilder.Entity<UserPersonal>(entity =>
+        {
+            entity.HasKey(up => up.Id);
+            entity.Property(up => up.Name).HasMaxLength(30);
+            entity.Property(up => up.Surname).HasMaxLength(30);
+            entity.Property(up => up.Bio).HasMaxLength(200);
+            entity.Property(up => up.ProfilePhoto).HasMaxLength(200);
+            entity.HasOne(up => up.User)
+                  .WithOne(u => u.UserPersonal)
+                  .HasForeignKey<UserPersonal>(up => up.UserId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // Configure UserPhysical
+        modelBuilder.Entity<UserPhysical>(entity =>
+        {
+            entity.HasKey(up => up.Id);
+            entity.Property(up => up.Height).HasColumnType("decimal(5,2)");
+            entity.Property(up => up.Weight).HasColumnType("decimal(5,2)");
+            entity.Property(up => up.Age).HasColumnType("int");
+            entity.Property(up => up.Birthday).HasColumnType("date");
+            entity.HasOne(up => up.User)
+                  .WithOne(u => u.UserPhysical)
+                  .HasForeignKey<UserPhysical>(up => up.UserId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
         // Configure User
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(u => u.Id);
-            entity.Property(u => u.Name).HasMaxLength(30);
-            entity.Property(u => u.Surname).HasMaxLength(30);
             entity.Property(u => u.Username).IsRequired().HasMaxLength(30);
             entity.HasIndex(u => u.Username).IsUnique();
             entity.Property(u => u.Email).IsRequired().HasMaxLength(40);
@@ -43,7 +84,6 @@ public class AppDbContext : DbContext
             entity.Property(u => u.Password).IsRequired();
             entity.Property(u => u.IsActive).HasConversion<string>();
             entity.Property(u => u.Role).HasConversion<string>();
-            entity.Property(u => u.Birthday).HasColumnType("date");
             entity.HasMany(entity => entity.FollowRequestsSent)
                   .WithOne(fr => fr.Requester)
                   .HasForeignKey(fr => fr.RequesterId)
@@ -78,8 +118,6 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Persona>(entity =>
         {
             entity.HasKey(p => p.Id);
-            entity.Property(p => p.Usertype)
-                  .HasConversion<string>();
             entity.HasOne(p => p.User)
                   .WithOne(u => u.UserPersona)
                   .HasForeignKey<Persona>(p => p.UserId)
@@ -100,7 +138,7 @@ public class AppDbContext : DbContext
                   .OnDelete(DeleteBehavior.Cascade);
         });
 
-        // Configure GarderobeCategory
+        // Configure Clothe Category
         modelBuilder.Entity<ClotheCategory>(entity =>
         {
             entity.HasKey(c => c.Id);
@@ -108,6 +146,17 @@ public class AppDbContext : DbContext
             entity.HasMany(c => c.Clothes)
                   .WithOne(cl => cl.Category)
                   .HasForeignKey(cl => cl.CategoryId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // Configure Clothe Category Type
+        modelBuilder.Entity<ClotheCategoryType>(entity =>
+        {
+            entity.HasKey(c => c.Id);
+            entity.Property(c => c.Name).IsRequired().HasMaxLength(50);
+            entity.HasMany(c => c.ClothesCategory)
+                  .WithOne(cl => cl.ClotheCategoryType)
+                  .HasForeignKey(cl => cl.ClotheCategoryTypeId)
                   .OnDelete(DeleteBehavior.Cascade);
         });
 
