be7dc20ad4d34b603d5cacd9f9e947a69aa1d380
sam <abdulsamed.kcygt@gmail.com>
2025-04-25 14:09:04 +0300

    renamed

--- Yconic.Application/Services/ClotheCategoryServices/ClotheCategoryService.cs
+++ Yconic.Application/Services/ClotheCategoryServices/ClotheCategoryService.cs
@@ -1,63 +1,65 @@
 ﻿using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Yconic.Domain.Dtos.ClotheCategoryDtos;
-using Yconic.Domain.Enums;
 using Yconic.Domain.Models;
 using Yconic.Domain.Wrapper;
-using Yconic.Infrastructure.Repositories.ClotheCategoriesRepositories;
+using Yconic.Infrastructure.Repositories.ClotheCategoryRepositories;
 using Yconic.Infrastructure.Repositories.GarderobeRepositories;
 
-namespace Yconic.Application.Services.ClotheCategoriesServices
+namespace Yconic.Application.Services.ClotheCategoryServices
 {
-    public class ClotheCategoriesService:IClotheCategoriesService
+    public class ClotheCategoryService : IClotheCategoryService
     {
-        protected readonly IClotheCategoriesRepository _clotheCategoriesRepository;
+        protected readonly IClotheCategoryRepository _clotheCategoriesRepository;
         protected readonly IGarderobeRepository _garderobeRepository;
         protected readonly IMapper _mapper;
-        public ClotheCategoriesService(IClotheCategoriesRepository clotheCategoriesRepository,IGarderobeRepository garderobeRepository,IMapper mapper)
+
+        public ClotheCategoryService(IClotheCategoryRepository clotheCategoriesRepository, IGarderobeRepository garderobeRepository, IMapper mapper)
         {
             _clotheCategoriesRepository = clotheCategoriesRepository;
             _garderobeRepository = garderobeRepository;
             _mapper = mapper;
         }
+
         public async Task<ApiResult<List<ClotheCategoryDto>>> GetAllClotheCategories()
         {
-            var clotheCategories =  await _clotheCategoriesRepository.GetAllClotheCategories();
+            var clotheCategories = await _clotheCategoriesRepository.GetAllClotheCategories();
             var mappedCategories = _mapper.Map<List<ClotheCategoryDto>>(clotheCategories);
             return ApiResult<List<ClotheCategoryDto>>.Success(mappedCategories);
         }
-        public async Task<ApiResult<ClotheCategoryDto>> GetClotheCategoriesById(Guid id)
+
+        public async Task<ApiResult<ClotheCategoryDto>> GetClotheCategoryById(Guid id)
         {
-            var category=  await _clotheCategoriesRepository.GetById(id);
+            var category = await _clotheCategoriesRepository.GetById(id);
             var mappedCategory = _mapper.Map<ClotheCategoryDto>(category);
             return ApiResult<ClotheCategoryDto>.Success(mappedCategory);
         }
-        public async Task<ApiResult<ClotheCategoryDto>> CreateClotheCategories(ClotheCategory clotheCategories)
+
+        public async Task<ApiResult<ClotheCategoryDto>> CreateClotheCategory(CreateClotheCategoryDto clotheCategory)
         {
-            var newClotheCategory = await _clotheCategoriesRepository.Add(clotheCategories);
+            var mappedCategory = _mapper.Map<ClotheCategory>(clotheCategory);
+            var newClotheCategory = await _clotheCategoriesRepository.Add(mappedCategory);
+
             var garderobe = await _garderobeRepository.GetById(newClotheCategory.GarderobeId);
-            if(garderobe.ClothesCategory == null)
+            if (garderobe.ClothesCategory == null)
             {
                 garderobe.ClothesCategory = new List<ClotheCategory>();
             }
             garderobe.ClothesCategory.Add(newClotheCategory);
 
             await _garderobeRepository.Update(garderobe);
-            
-            var mappedCategory = _mapper.Map<ClotheCategoryDto>(newClotheCategory);
 
-            return ApiResult<ClotheCategoryDto>.Success(mappedCategory);
+            var mappedCategoryDto = _mapper.Map<ClotheCategoryDto>(newClotheCategory);
+
+            return ApiResult<ClotheCategoryDto>.Success(mappedCategoryDto);
         }
-        public async Task<ApiResult<ClotheCategoryDto>> UpdateClotheCategories(ClotheCategory clotheCategories)
+
+        public async Task<ApiResult<ClotheCategoryDto>> UpdateClotheCategory(ClotheCategory clotheCategories)
         {
-            var category =  await _clotheCategoriesRepository.Update(clotheCategories);
+            var category = await _clotheCategoriesRepository.Update(clotheCategories);
             var mappedCategory = _mapper.Map<ClotheCategoryDto>(category);
             return ApiResult<ClotheCategoryDto>.Success(mappedCategory);
         }
+
         public async Task<ApiResult<ClotheCategoryDto>> UpdateClotheCategoryWithPatch(Guid id, UpdateClotheCategoryDto dto)
         {
             var existing = await _clotheCategoriesRepository.GetById(id);
@@ -65,24 +67,25 @@ namespace Yconic.Application.Services.ClotheCategoriesServices
                 throw new Exception("Category not found");
 
             if (!string.IsNullOrWhiteSpace(dto.Name))
+
                 existing.Name = dto.Name;
 
-            if (dto.CategoryType.HasValue)
-                existing.CategoryType = (CategoryTypes)dto.CategoryType.Value;
+            if (dto.CategoryTypeId.HasValue)
+                existing.ClotheCategoryTypeId = dto.CategoryTypeId.Value;
 
             var updatedCategory = await _clotheCategoriesRepository.Update(existing);
             var mappedCategory = _mapper.Map<ClotheCategoryDto>(updatedCategory);
-            
+
             return ApiResult<ClotheCategoryDto>.Success(mappedCategory);
         }
 
-        public async Task DeleteClotheCategories(Guid id)
+        public async Task DeleteClotheCategory(Guid id)
         {
             var clotheCategory = await _clotheCategoriesRepository.GetClotheCategoryById(id);
 
             if (clotheCategory.Clothes.Count > 0)
             {
-                foreach(var clothe in clotheCategory.Clothes)
+                foreach (var clothe in clotheCategory.Clothes)
                 {
                     foreach (var photo in clothe.Photos)
                     {
---
+++ Yconic.Application/Services/ClotheCategoryServices/IClotheCategoryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Dtos.ClotheCategoryDtos;
+using Yconic.Domain.Models;
+using Yconic.Domain.Wrapper;
+
+namespace Yconic.Application.Services.ClotheCategoryServices
+{
+    public interface IClotheCategoryService
+    {
+        Task<ApiResult<List<ClotheCategoryDto>>> GetAllClotheCategories();
+
+        Task<ApiResult<ClotheCategoryDto>> GetClotheCategoryById(Guid id);
+
+        Task<ApiResult<ClotheCategoryDto>> CreateClotheCategory(CreateClotheCategoryDto clotheCategories);
+
+        Task<ApiResult<ClotheCategoryDto>> UpdateClotheCategory(ClotheCategory clotheCategories);
+
+        Task<ApiResult<ClotheCategoryDto>> UpdateClotheCategoryWithPatch(Guid id, UpdateClotheCategoryDto clotheCategoryDto);
+
+        Task DeleteClotheCategory(Guid id);
+    }
+}
