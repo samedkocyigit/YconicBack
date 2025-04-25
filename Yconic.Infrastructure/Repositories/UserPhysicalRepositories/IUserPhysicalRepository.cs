ae5a8a81c74467da44f4d5fa53648d0675f27395
sam <abdulsamed.kcygt@gmail.com>
2025-04-25 14:19:43 +0300

    new queries has been defined and implemented

--- Yconic.Infrastructure/Repositories/UserPhysicalRepositories/IUserPhysicalRepository.cs
+++ Yconic.Infrastructure/Repositories/UserPhysicalRepositories/IUserPhysicalRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Yconic.Domain.Models.UserModels;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
 
 namespace Yconic.Infrastructure.Repositories.UserPhysicalRepositories
 {
-    internal interface IUserPhysicalRepository
+    public interface IUserPhysicalRepository : IGenericRepository<UserPhysical>
     {
     }
 }
