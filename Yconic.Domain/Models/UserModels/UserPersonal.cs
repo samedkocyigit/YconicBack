a20eb8aa8f79d2c55f0cd22a9e9652f74b44ab52
sam <abdulsamed.kcygt@gmail.com>
2025-04-25 14:14:01 +0300

    user property has been ignored

--- Yconic.Domain/Models/UserModels/UserPersonal.cs
+++ Yconic.Domain/Models/UserModels/UserPersonal.cs
@@ -1,8 +1,12 @@
-﻿namespace Yconic.Domain.Models.UserModels
+﻿using System.Text.Json.Serialization;
+
+namespace Yconic.Domain.Models.UserModels
 {
     public class UserPersonal : BaseClass
     {
         public Guid UserId { get; set; }
+
+        [JsonIgnore]
         public User? User { get; set; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
