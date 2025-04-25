4f61008b251721abb7a0281ace63310ed6ea6a8f
sam <abdulsamed.kcygt@gmail.com>
2025-04-25 14:17:02 +0300

    new repos funcs has been defined and implemented

---
+++ Yconic.Infrastructure/Repositories/ClotheCategoryTypeRepositories/ClotheCategoryTypeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yconic.Domain.Models;
+using Yconic.Infrastructure.ApplicationDbContext;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.ClotheCategoryTypeRepositories
+{
+    public class ClotheCategoryTypeRepository : GenericRepository<ClotheCategoryType>, IClotheCategoryTypeRepository
+    {
+        protected readonly AppDbContext _context;
+
+        public ClotheCategoryTypeRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Yconic.Infrastructure/Repositories/PersonaTypeRopositories/PersonaTypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Yconic.Domain.Models;
+using Yconic.Infrastructure.ApplicationDbContext;
+using Yconic.Infrastructure.Repositories.GenericRepositories;
+
+namespace Yconic.Infrastructure.Repositories.PersonaTypeRopositories
+{
+    public class PersonaTypeRepository : GenericRepository<PersonaType>, IPersonaTypeRepository
+    {
+        protected readonly AppDbContext _context;
+
+        public PersonaTypeRepository(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
